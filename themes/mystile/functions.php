3b8ec86646ee1f24ef3950661cf257fb23302bdd
Andrew Reifman <andrewr23@gmail.com>
2014-07-17 13:04:37 -0400

    added wp-content files

---
+++ themes/mystile/functions.php
@@ -0,0 +1,123 @@
+<?php
+// File Security Check
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( 'You do not have sufficient permissions to access this page!' );
+}
+?>
+<?php
+
+/*-----------------------------------------------------------------------------------*/
+/* Start WooThemes Functions - Please refrain from editing this section */
+/*-----------------------------------------------------------------------------------*/
+
+// Define the theme-specific key to be sent to PressTrends.
+define( 'WOO_PRESSTRENDS_THEMEKEY', 'zdmv5lp26tfbp7jcwiw51ix9sj389e712' );
+
+// WooFramework init
+require_once ( get_template_directory() . '/functions/admin-init.php' );
+
+/*-----------------------------------------------------------------------------------*/
+/* Load the theme-specific files, with support for overriding via a child theme.
+/*-----------------------------------------------------------------------------------*/
+
+$includes = array(
+				'includes/theme-options.php', 			// Options panel settings and custom settings
+				'includes/theme-functions.php', 		// Custom theme functions
+				'includes/theme-actions.php', 			// Theme actions & user defined hooks
+				'includes/theme-comments.php', 			// Custom comments/pingback loop
+				'includes/theme-js.php', 				// Load JavaScript via wp_enqueue_script
+				'includes/sidebar-init.php', 			// Initialize widgetized areas
+				'includes/theme-widgets.php',			// Theme widgets
+				'includes/theme-install.php',			// Theme installation
+				'includes/theme-woocommerce.php',		// WooCommerce options
+				'includes/theme-plugin-integrations.php'	// Plugin integrations
+				);
+
+// Allow child themes/plugins to add widgets to be loaded.
+$includes = apply_filters( 'woo_includes', $includes );
+
+foreach ( $includes as $i ) {
+	locate_template( $i, true );
+}
+
+/*-----------------------------------------------------------------------------------*/
+/* You can add custom functions below */
+/*-----------------------------------------------------------------------------------*/
+
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_product-fields',
+		'title' => 'Product Fields',
+		'fields' => array (
+			array (
+				'key' => 'field_53c4929134815',
+				'label' => 'Browser Support',
+				'name' => 'browser_support',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'product',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'page',
+					'order_no' => 0,
+					'group_no' => 1,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
+
+
+add_action( 'woocommerce_single_product_summary', 'wc_product_sold_count', 11 );
+function wc_product_sold_count() {
+	global $product;
+	$units_sold = get_post_meta( $product->id, 'total_sales', true );
+	echo '<p>' . sprintf( __( 'Units Sold: %s', 'woocommerce' ), $units_sold ) . '</p>';
+}
+
+
+// Hook in
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+
+// Our hooked in function - $fields is passed via the filter!
+function custom_override_checkout_fields( $fields ) {
+     unset($fields['order']['order_comments']);
+     unset($fields['billing']['billing_country']);
+     unset($fields['billing']['billing_address_1']);
+     unset($fields['billing']['billing_address_2']);
+     unset($fields['billing']['billing_city']);
+     unset($fields['billing']['billing_postcode']);
+     unset($fields['billing']['billing_state']);
+          unset($fields['billing']['billing_phone']);
+     return $fields;
+}
+/*-----------------------------------------------------------------------------------*/
+/* Don't add any code below here or the sky will fall down */
+/*-----------------------------------------------------------------------------------*/
+?>
