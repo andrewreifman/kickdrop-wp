49bb0eb595d0eafa4e4fc1bc765c0dfae9415a89
Andrew Reifman <andrewr23@gmail.com>
2014-07-17 22:40:13 -0400

    added sass folder and config.rb for sass compiling

--- themes/mystile/header.php
+++ themes/mystile/header.php
@@ -35,68 +35,55 @@ global $woo_options, $woocommerce;
 <?php woo_top(); ?>
 
 <div id="wrapper">
-
-
-
 	<div id="top">
-		<nav class="col-full" role="navigation">
+		<nav role="navigation" class="clearfix">
 			<?php if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'top-menu' ) ) { ?>
-			<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'top-nav', 'menu_class' => 'nav fl', 'theme_location' => 'top-menu' ) ); ?>
+			<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'top-nav', 'menu_class' => 'nav pull-right', 'theme_location' => 'top-menu' ) ); ?>
 			<?php } ?>
-			<?php
-				if ( class_exists( 'woocommerce' ) ) {
-					echo '<ul class="nav wc-nav">';
-					woocommerce_cart_link();
-					echo '<li class="checkout"><a href="'.esc_url($woocommerce->cart->get_checkout_url()).'">'.__('Checkout','woothemes').'</a></li>';
-					echo get_search_form();
-					echo '</ul>';
-				}
-			?>
 		</nav>
 	</div><!-- /#top -->
-
-
 
     <?php woo_header_before(); ?>
 
 	<header id="header" class="col-full">
-
-
-
-	    <hgroup>
-
-	    	 <?php
-			    $logo = esc_url( get_template_directory_uri() . '/images/logo.png' );
+	  <hgroup>
+	    <?php
+			  $logo = esc_url( get_template_directory_uri() . '/images/logo.png' );
 				if ( isset( $woo_options['woo_logo'] ) && $woo_options['woo_logo'] != '' ) { $logo = $woo_options['woo_logo']; }
 				if ( isset( $woo_options['woo_logo'] ) && $woo_options['woo_logo'] != '' && is_ssl() ) { $logo = preg_replace("/^http:/", "https:", $woo_options['woo_logo']); }
 			?>
 			<?php if ( ! isset( $woo_options['woo_texttitle'] ) || $woo_options['woo_texttitle'] != 'true' ) { ?>
 			    <a id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr( get_bloginfo( 'description' ) ); ?>">
-			    	<img src="<?php echo $logo; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
+			    	<img src="<?php echo $logo; ?>" width="134" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
 			    </a>
 		    <?php } ?>
 
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			<h3 class="nav-toggle"><a href="#navigation"><mark class="websymbols">&#178;</mark> <span><?php _e('Navigation', 'woothemes'); ?></span></a></h3>
-
 		</hgroup>
 
-        <?php woo_nav_before(); ?>
+		<?php
+			if ( class_exists( 'woocommerce' ) ) {
+				echo '<ul class="nav pull-right">';
+				woocommerce_cart_link();
+				echo get_search_form();
+				echo '</ul>';
+			}
+		?>
 
-		<nav id="navigation" class="col-full" role="navigation">
+    <?php woo_nav_before(); ?>
 
+		<nav id="navigation" role="navigation">
 			<?php
-			if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'primary-menu' ) ) {
-				wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fr', 'theme_location' => 'primary-menu' ) );
-			} else {
+				if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'primary-menu' ) ) {
+					wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fr', 'theme_location' => 'primary-menu' ) );
+				} else {
 			?>
-	        <ul id="main-nav" class="nav fl">
-				<?php if ( is_page() ) $highlight = 'page_item'; else $highlight = 'page_item current_page_item'; ?>
-				<li class="<?php echo $highlight; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php _e( 'Home', 'woothemes' ); ?></a></li>
-				<?php wp_list_pages( 'sort_column=menu_order&depth=6&title_li=&exclude=' ); ?>
-			</ul><!-- /#nav -->
-	        <?php } ?>
+	       <ul id="main-nav" class="nav">
+					<?php if ( is_page() ) $highlight = 'page_item'; else $highlight = 'page_item current_page_item'; ?>
+						<li class="<?php echo $highlight; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php _e( 'Home', 'woothemes' ); ?></a></li>
+					<?php wp_list_pages( 'sort_column=menu_order&depth=6&title_li=&exclude=' ); ?>
+				</ul><!-- /#nav -->
+	    <?php } ?>
 
 		</nav><!-- /#navigation -->
 
