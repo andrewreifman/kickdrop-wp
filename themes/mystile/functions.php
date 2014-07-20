00e015c2bbdc1a012281cac2727c5739d5be0ea0
Bilal Mechairia <me@bilalmechairia.com>
2014-07-20 12:37:57 -0400

    templatechanges

--- themes/mystile/functions.php
+++ themes/mystile/functions.php
@@ -164,45 +164,7 @@ function hide_free_price_notice( $price ) {
   return '';
 }
 
-/**
- * Change the add to cart text on single product pages
- */
-add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );
 
-function woo_custom_cart_button_text() {
-
-	global $woocommerce;
-	
-	foreach($woocommerce->cart->get_cart() as $cart_item_key => $values ) {
-		$_product = $values['data'];
-	
-		if( get_the_ID() == $_product->id ) {
-			return __('Already in cart', 'woocommerce');
-		}
-	}
-	
-	return __('Add to cart', 'woocommerce');
-}
-
-/**
- * Change the add to cart text on product archives
- */
-add_filter( 'add_to_cart_text', 'woo_archive_custom_cart_button_text' );
-
-function woo_archive_custom_cart_button_text() {
-
-	global $woocommerce;
-	
-	foreach($woocommerce->cart->get_cart() as $cart_item_key => $values ) {
-		$_product = $values['data'];
-	
-		if( get_the_ID() == $_product->id ) {
-			return __('Already in cart', 'woocommerce');
-		}
-	}
-	
-	return __('Add to cart', 'woocommerce');
-}
 
 add_filter( 'woocommerce_subcategory_count_html', 'woo_remove_category_products_count' );
 
