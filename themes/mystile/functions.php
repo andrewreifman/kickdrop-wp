ed0de7a7cca91fc1d7d8381c6129daf4957ede3b
Andrew Reifman <andrewr23@gmail.com>
2014-07-23 22:44:30 -0400

    styling checkout page

--- themes/mystile/functions.php
+++ themes/mystile/functions.php
@@ -159,7 +159,6 @@ add_filter( 'woocommerce_variation_free_price_html', 'hide_free_price_notice' );
  * Hides the 'Free!' price notice
  */
 function hide_free_price_notice( $price ) {
-
   return 'FREE';
 }
 
